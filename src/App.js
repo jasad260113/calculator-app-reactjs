cdd6f292e0853ece82fe6cf4dd823dc2900eba75
Maurice Jaso <mauricejaso@outlook.com>
2018-04-25 14:02:36 +0200

    Update to app

--- src/App.js
+++ src/App.js
@@ -7,51 +7,89 @@ constructor (props) {
   this.state = {
     isPowerOn: false,
     isHidden: true,
-    displayNumber: "0"
+    displayNumber: "0",
+    operator: "",
+    lastButton: "",
   };
 
   this.checkPowerClick = this.checkPowerClick.bind(this);
   this.deleteAllClick = this.deleteAllClick.bind(this);
   this.deleteClick = this.deleteClick.bind(this);
   this.checkNumberClick = this.checkNumberClick.bind(this);
+  this.checkOperButton = this.checkOperButton.bind(this);
   this.checkEqualClick = this.checkEqualClick.bind(this);
 }
 
 checkPowerClick() {
+  if (this.state.isPowerOn === true) {
+    this.setState({
+      displayNumber: "0"
+    })
+  }
   this.setState(prevState => ({
         isPowerOn: !prevState.isPowerOn,
-        isHidden: !this.state.isHidden
+        isHidden: !this.state.isHidden,
+        lastButton: "power"
   }));
 };
 
 deleteAllClick() {
   this.setState({
-    displayNumber: "0"
+    displayNumber: "0",
+    lastButton: "deleteAll"
   })
 };
 
 deleteClick() {
   this.setState({
-    displayNumber: "0"
+    displayNumber: "0",
+    lastButton: "delete"
   })
-}
+};
 
 checkNumberClick(number) {
+
+let displayNumber = this.state.displayNumber;
+let lastButton = this.state.lastButton;
+
+  if (displayNumber === "0" || lastButton === "equal") {
+    let nonZero = displayNumber = "";
+    let newNumber = nonZero + number.toString();
+    this.setState({
+      displayNumber: newNumber,
+      lastButton: "number"
+    });
+  }
+  else if (displayNumber !== "0") {
+    this.setState({
+      displayNumber: displayNumber + number,
+      lastButton: "number"
+    })
+
+  };
+};
+
+checkOperButton (operator) {
   this.setState({
-    displayNumber: number
-  });
+    operator: operator,
+    displayNumber: this.state.displayNumber + operator,
+    lastButton: "operator"
+  })
 };
 
 checkEqualClick() {
-
-}
+  this.setState({
+    displayNumber: eval(this.state.displayNumber),
+    lastButton: "equal"
+  })
+};
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"> Calculator </h1>
-        </header>
+        {/* <header className="App-header">
+          <h1 className="App-title"></h1>
+        </header> */}
         <div className="calculator">
           <div className="screen">
             <Display displayNumber={this.state.displayNumber}/>
@@ -62,26 +100,26 @@ checkEqualClick() {
           <div className="buttons">
             <ClearButton clear="AC" power={this.state.isPowerOn} onClick={this.deleteAllClick}/>
             <ClearButton clear="CE" power={this.state.isPowerOn} onClick={this.deleteClick}/>
-            <OperButton operator="+" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
-            <OperButton operator="-" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
+            <OperButton operator="+" power={this.state.isPowerOn} onClick={this.checkOperButton}/>
+            <OperButton operator="-" power={this.state.isPowerOn} onClick={this.checkOperButton}/>
           </div>
           <div className="buttons">
             <NumButton number="7" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
             <NumButton number="8" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
             <NumButton number="9" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
-            <OperButton operator="*" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
+            <OperButton operator="*" power={this.state.isPowerOn} onClick={this.checkOperButton}/>
           </div>
           <div className="buttons">
             <NumButton number="4" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
             <NumButton number="5" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
             <NumButton number="6" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
-            <OperButton operator="/" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
+            <OperButton operator="/" power={this.state.isPowerOn} onClick={this.checkOperButton}/>
           </div>
           <div className="buttons">
             <NumButton number="1" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
             <NumButton number="2" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
             <NumButton number="3" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
-            <NumButton number="%" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
+            <OperButton operator="%" power={this.state.isPowerOn} onClick={this.checkOperButton}/>
           </div>
           <div className="buttons">
             <NumButton number="0" power={this.state.isPowerOn} onClick={this.checkNumberClick}/>
@@ -99,16 +137,15 @@ class PowerButton extends Component {
     super(props);
     this.clicked = this.clicked.bind(this);
   }
-
   clicked(e) {
 		e.preventDefault();
 		this.props.onClick(this.props.power);
-	}
-
+	};
   render() {
-    return ( <button type="button" className="power" onClick={this.clicked}> {this.props.power ? "Turn Off" : "Turn On"} </button> );
+    let bgColor = this.props.power ? "red" : "green";
+    return ( <button type="button" className="power" onClick={this.clicked} style={{backgroundColor: bgColor}}> {this.props.power ? "Turn Off" : "Turn On"} </button> );
   }
-}
+};
 
 class Display extends Component {
   render() {
@@ -140,7 +177,7 @@ class OperButton extends Component {
     this.props.onClick(this.props.operator)
   }
   render() {
-    return <button type="button" disabled={!this.props.power} onClick={this.clicked}> {this.props.operator} </button>;
+    return ( <button type="button" disabled={!this.props.power} onClick={this.clicked}> {this.props.operator} </button> );
   }
 };
 
@@ -154,25 +191,21 @@ class ClearButton extends Component {
     this.props.onClick(this.props.clear)
   }
   render() {
-    return <button type="button" disabled={!this.props.power} onClick={this.clicked}> {this.props.clear} </button>;
+    return ( <button type="button" disabled={!this.props.power} onClick={this.clicked}> {this.props.clear} </button> );
   }
-}
+};
 
 class EqualButton extends React.Component {
 	constructor(props) {
 		super(props);
 		this.clicked = this.clicked.bind(this);
 	}
-
 	clicked(e){
 		e.preventDefault();
 		this.props.onClick();
 	}
-
 	render() {
-		return (
-			<button type="button" className="equal" onClick={this.clicked}>=</button>
-		);
+		return ( <button type="button" className="equal" onClick={this.clicked}>=</button> );
 	}
 };
 
